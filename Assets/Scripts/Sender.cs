853a4262a65870a7ccb1b1b3eaf50c92ae928863
Marius Bejenuta <marius@Mariuss-MacBook-Pro.local>
2023-04-17 14:12:09 +0300

    Adaugat feature de transmitere sincrona a datelor despre rotatia articulatiilor pe serial port la brat, lucreaza.

---
+++ Assets/Scripts/Sender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Ports;
+using UnityEngine;
+
+namespace SerialComm
+
+{ 
+
+        public class Sender
+        {
+        public void SendGCode(SerialPort port, string command)
+        {
+                
+                port.WriteLine(command);
+                Debug.Log($"Trimitem pe {port} : {command}");
+
+        }
+
+    }
+}
+
